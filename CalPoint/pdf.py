7aecfc6e9afa4da178dd056496747ab0efb85fc1
Unbeknownst_47_max <ankit.thakker@somaiya.edu>
2020-04-27 16:53:15 +0530

    Removed unnecessary files

--- CalPoint/pdf.py
+++
@@ -1,5 +0,0 @@
-import tabula
-
-tabula.convert_into("FIRSTSEMMARKS.pdf","FSM.csv",pages="all")
-tabula.convert_into("SECONDSEMMARKS.pdf","SSM.csv",pages="all")
-tabula.convert_into("THIRDSEMMARKS.pdf","TSM.csv",pages="all")
